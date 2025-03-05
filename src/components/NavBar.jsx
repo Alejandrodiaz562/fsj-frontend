2d3130c6307984d65ec4ee6a26475c0b5a5f8af3
Alejandrodiaz562 <111014821+Alejandrodiaz562@users.noreply.github.com>
2025-03-04 23:00:14 -0500

    fixing errors

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,11 +16,11 @@ const NavBar = ({categories}) => {
         document.body.style.overflow = "auto";
     }
 
-    const menuOpenClasses = 'shadow-[2px_5px_500px_5px_black] border-l-1 border-white bg-myblue pt-20 fixed  right-0  top-0 w-[65%] h-[100%] flex flex-col items-center transition-transform duration-800 ease-in-out'
+    const menuOpenClasses = 'shadow-[2px_5px_500px_5px_black] border-l-1 border-white bg-myblue pt-20 fixed  right-0  top-0 w-[65%] h-[100%] flex flex-col items-center transition-transform duration-800 ease-in-out sm:left-[-20px] sm:bottom-[40px]'
 
-    const menuCloseClasses = 'border-l-1 border-white bg-myblue pt-20 fixed right-0 top-0 w-[65%] h-[100%] flex flex-col items-center translate-x-[300px] transition-transform duration-800 ease-in-out'
+    const menuCloseClasses = 'border-l-1 border-white bg-myblue pt-20 fixed right-0 top-0 w-[65%] h-[100%] flex flex-col items-center translate-x-[300px] transition-transform duration-800 ease-in-out sm:left-[-330px] sm:bottom-[40px] '
 
-    const desktopClasses = 'sm:flex sm:flex-row sm:bg-transparent sm:relative sm:top-auto sm:right-auto  sm:left-[-250px] sm:bottom-[40px] sm:border-none'
+    const desktopClasses = 'sm:flex sm:flex-row sm:bg-transparent sm:relative sm:top-auto sm:right-auto   sm:border-none'
 
     return ( 
         <nav className= "bg-myblue text-white h-[10vh]  flex justify-between items-center px-[5%] sticky top-0 z-[50]" >
